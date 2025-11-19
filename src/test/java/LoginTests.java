e1730be59ac6415e468b0eade091f342b152e50e
Zaid Khan <59452329+ZaidKhan02@users.noreply.github.com>
2025-11-19 14:43:24 -0500

    Selenium and TestNG practice

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -8,18 +8,77 @@ import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
-    public void loginEmptyEmailPassword() {
+    public void loginValidEmailPassword() throws InterruptedException {
+        // Pre-condition: chromedriver is set up by BaseTest
+        // I dont think we call setupClass, launchBrower, closebrowser becuase those are
+        // beforesuite, beforemethod, aftermethod
+        navigateToPage();
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+        provideEmail("demo@koel.dev");
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        providePassword("demo");
 
-        String url = "https://demo.koel.dev/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+        clickLogin();
+
+        Thread.sleep(2000);
+
+        isAvatarDisplayed();
+
     }
+
+    @Test
+    public void changeProfileName() throws InterruptedException {
+        loginAsUser();
+
+        clickAvatarIcon();
+        Thread.sleep(2000);
+
+        String randomName = generateRandomName();
+        System.out.println("The random name is " + randomName);
+
+        provideCurrentPassword("demo");
+        Thread.sleep(2000);
+
+        provideProfileName(randomName);
+        Thread.sleep(2000);
+
+        provideEmail("demo@koel.dev");
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void loginInvalidEmailValidPassword() throws InterruptedException {
+        // Pre-condition: chromedriver is set up by BaseTest
+        // I dont think we call setupClass, launchBrower, closebrowser becuase those are
+        // beforesuite, beforemethod, aftermethod
+        navigateToPage();
+
+        provideEmail("demo@joel.dev");
+
+        providePassword("demo");
+
+        clickLogin();
+        Thread.sleep(2000);
+
+        /* If I remov these lines, it will pass */
+        isAvatarDisplayed();
+    }
+
+    @Test
+    public void loginValidEmailEmptyPassword() throws InterruptedException {
+        // Pre-condition: chromedriver is set up by BaseTest
+        // I dont think we call setupClass, launchBrower, closebrowser becuase those are
+        // beforesuite, beforemethod, aftermethod
+        navigateToPage();
+
+        provideEmail("demo@joel.dev");
+
+        clickLogin();
+
+        Thread.sleep(2000);
+
+        /* If I remov these lines, it will pass */
+        isAvatarDisplayed();
+    }
+
 }
---
+++ src/test/java/NegativeLoginTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class NegativeLoginTest extends BaseTest {
+
+    @Test
+    public void loginInvalidEmailValidPassword() throws InterruptedException {
+        // Pre-condition: chromedriver is set up by BaseTest
+        // Pre-condition: chromedriver is set up by BaseTest
+        // I dont think we call setupClass, launchBrower, closebrowser becuase those are
+        // beforesuite, beforemethod, aftermethod
+        navigateToPage();
+
+        provideEmail("demo@joel.dev");
+
+        providePassword("demo");
+
+        clickLogin();
+        Thread.sleep(2000);
+
+        /* If I remov these lines, it will pass */
+        isAvatarDisplayed();
+    }
+}
